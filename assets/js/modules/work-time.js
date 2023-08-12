1043d39d70125efe4e266481281193c34cc1fab1
YuriSouzaDev <yurisouza.developer@gmail.com>
2023-08-12 02:18:46 -0300

    :bug: fix:contact height bug

--- assets/js/modules/work-time.js
+++ assets/js/modules/work-time.js
@@ -1,16 +1,13 @@
 export default function initWorkTime() {
-  const workTime = document.querySelector("[data-week]");
-  const weekDay = workTime.dataset.week.split(",").map(Number);
+  const workTime = document.querySelector("[data-time]");
   const weekTime = workTime.dataset.time.split(",").map(Number);
 
   const date = new Date();
-  const actualDay = date.getDay();
   const actualTime = date.getHours();
 
-  const weekIsOpen = weekDay.indexOf(actualDay) !== -1;
   const timeIsOpen = actualTime >= weekTime[0] && actualTime < weekTime[1];
 
-  if (weekIsOpen && timeIsOpen) {
+  if (timeIsOpen) {
     workTime.classList.add("open");
   }
 }
